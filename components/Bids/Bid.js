1112a13fb671817e60656d8a8fed82f94f904d8e
Yashank <yashank.varshney@cern.ch>
2018-11-18 04:39:25 -0800

    Fixed Component and Added to Bid Screen

--- components/Bids/Bid.js
+++ components/Bids/Bid.js
@@ -1,14 +1,39 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+import { Button, Card, Title, Paragraph } from "react-native-paper";
 
 export default class Bid extends React.Component {
   render() {
     const data = this.props.navigation.state.params.data;
-    console.log(data);
     return (
-      <View>
-        <Text>DISPLAY BID DETAILS HERE</Text>
+      <View style={styles.container}>
+        <Card style={styles.card}>
+          <Card.Cover source={{ uri: data.product_img }} />
+          <Card.Content style={styles.content}>
+            <Title>{data.product_name}</Title>
+            <Paragraph>{data.product_bid}</Paragraph>
+          </Card.Content>
+          <Card.Actions style={styles.content}>
+            <Button>Cancel</Button>
+            <Button>Ok</Button>
+          </Card.Actions>
+        </Card>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    backgroundColor: "#fff"
+  },
+  card: {
+    elevation: 4,
+    margin: 10
+  },
+  content: {
+    alignItems: "center"
+  }
+});
